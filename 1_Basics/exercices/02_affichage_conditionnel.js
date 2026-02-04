56ea348dba7dbb97d6906a7fdefa83ae35216c02
Amina <aminata.fnd@gmail.fr>
2026-02-04 08:56:12 +0100

    exercice 2

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -2,4 +2,11 @@
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
 const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+const boutique_ouverte = false;
+
+if(boutique_ouverte){
+    console.log(`Bienvenue dans la boutique ${nom_boutique} Aventurier ! 🎉`)
+}
+else {
+    console.log(`La boutique ${nom_boutique} est fermée, revenez plus tard Aventurier ! 😴`)
+}
